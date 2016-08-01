8de352f4ba7194621f957ca3c9a2e971e20ac6b2
David Ladowitz <david@ladowitz.com>
2016-08-01 12:50:20 -0700

    finishing up

---
+++ gilded_rose.rb
@@ -0,0 +1,99 @@
+class GildedRose
+  SPECIALTY_ITEMS = ["Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", "Conjured Mana Cake"]
+  STATIC_ITEMS = ["Sulfuras, Hand of Ragnaros"]
+  GENERAL_QUALITY_RATE = 1
+
+  def initialize(items)
+    @items = items
+  end
+
+  def update_quality()
+    @items.each do |item|
+      if SPECIALTY_ITEMS.include?(item.name)
+        specialty_quality_rules(item)
+      else
+        general_quality_rules(item)
+      end
+
+      validate_min_max_quality(item)
+      decrement_sell_in(item)
+    end
+  end
+
+  def general_quality_rules(item)
+    if item.sell_in >= 1
+      item.quality -= GENERAL_QUALITY_RATE
+    else
+      item.quality -= GENERAL_QUALITY_RATE * 2
+    end
+  end
+
+  def specialty_quality_rules(item)
+    #given more time I'd build a method to dynamically look at names and route to methods
+    case item.name
+    when "Aged Brie"
+      brie(item)
+    when "Sulfuras, Hand of Ragnaros"
+      sulfuras(item)
+    when "Backstage passes to a TAFKAL80ETC concert"
+      backstage_passes(item)
+    when "Conjured Mana Cake"
+      conjured(item)
+    else
+      puts "Not sure what the item is"
+    end
+  end
+
+  def validate_min_max_quality(item)
+    # never let quality drop below 0, except for static values
+    if STATIC_ITEMS.include? item.name
+      return
+    elsif item.quality < 0
+      item.quality = 0
+    elsif item.quality > 50
+      item.quality = 50
+    end
+  end
+
+  def decrement_sell_in(item)
+    item.sell_in -= 1 unless STATIC_ITEMS.include? item.name
+  end
+
+  def brie(item)
+    item.quality += GENERAL_QUALITY_RATE
+  end
+
+  def conjured(item)
+    item.sell_in >= 1 ? item.quality -= GENERAL_QUALITY_RATE * 2 : item.quality -= GENERAL_QUALITY_RATE * 4
+  end
+
+  def backstage_passes(item)
+    if item.sell_in <= 0
+      item.quality = 0
+    elsif item.sell_in <= 5
+      item.quality += GENERAL_QUALITY_RATE * 3
+    elsif item.sell_in <= 10
+      item.quality += GENERAL_QUALITY_RATE * 2
+    else
+      item.quality += GENERAL_QUALITY_RATE * 1
+    end
+  end
+
+  def sulfuras(item)
+    "I shall not be changed"
+  end
+end
+
+class Item
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def to_s()
+    "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
---
+++ gilded_rose_tests.rb
@@ -0,0 +1,109 @@
+require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'test/unit'
+
+class TestUntitled < Test::Unit::TestCase
+
+  def test_foo
+    items = [Item.new("foo", 0, 0)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].name, "foo"
+  end
+
+  def test_general_items_before_expiration
+    items = [Item.new("Dexterity Vest", 10, 20)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, 9
+    assert_equal items[0].quality, 19
+  end
+
+  def test_general_items_after_expiration
+    items = [Item.new("Dexterity Vest", 10, 20)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, 9
+    assert_equal items[0].quality, 19
+  end
+
+  def test_general_items_min_quality
+    items = [Item.new("Dexterity Vest", -100, 0)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, -101
+    assert_equal items[0].quality, 0
+  end
+
+  def test_brie_before_expire
+    items = [Item.new("Aged Brie", 2, 0)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, 1
+    assert_equal items[0].quality, 1
+  end
+
+  def test_brie_after_expire
+    items = [Item.new("Aged Brie", -1, 0)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, -2
+    assert_equal items[0].quality, 1
+  end
+
+  def test_Sulfuras
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, 0
+    assert_equal items[0].quality, 80
+  end
+
+  def test_Sulfuras_after_expiration
+    items = [Item.new("Sulfuras, Hand of Ragnaros", -1, 80)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, -1
+    assert_equal items[0].quality, 80
+  end
+
+  def test_passes_15_days
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, 14
+    assert_equal items[0].quality, 21
+  end
+
+  def test_passes_10_days
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 20)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, 9
+    assert_equal items[0].quality, 22
+  end
+
+  def test_passes_5_days
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 20)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, 4
+    assert_equal items[0].quality, 23
+  end
+
+  def test_passes_after_expiration
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, -1
+    assert_equal items[0].quality, 0
+  end
+
+  def test_conjured_before_expiration
+    items = [Item.new("Conjured Mana Cake", 3, 6)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, 2
+    assert_equal items[0].quality, 4
+  end
+
+  def test_conjured_after_expiration
+    items = [Item.new("Conjured Mana Cake", 0, 6)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, -1
+    assert_equal items[0].quality, 2
+  end
+
+  def test_specialty_min
+    items = [Item.new("Conjured Mana Cake", 1, 0)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].sell_in, 0
+    assert_equal items[0].quality, 0
+  end
+end
